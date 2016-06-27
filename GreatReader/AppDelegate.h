9e2643f3762149e72e11b63807b5f0d8006a75cf
mobyzhang <496843549@qq.com>
2016-06-27 15:10:58 +0800

    source code
    RT

---
+++ GreatReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GreatReader
+//
+//  Created by MIYAMOTO Shohei on 2013/12/17.
+//  Copyright (c) 2014 MIYAMOTO Shohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pods/Target Support Files/Pods-GreatReader/Pods-GreatReader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GreatReader : NSObject
+@end
+@implementation PodsDummy_Pods_GreatReader
+@end
